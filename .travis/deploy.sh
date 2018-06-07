ea617e763b366185079bba9007bcc140209c72b2
shudarshon <shudarshon@upskillable.com>
2018-06-07 13:37:54 +0600

    testing alternate deployment without ssh knownhost

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
-#rsync -r --delete-after --quiet -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" $TRAVIS_BUILD_DIR/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH #discouraged to apply
+#rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+rsync -r --delete-after --quiet -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" $TRAVIS_BUILD_DIR/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH #discouraged to apply
